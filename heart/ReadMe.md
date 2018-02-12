ded475857c1f8570b87349fa7a85107bda1edc97
Michael Clark <micl@umich.edu>
2018-02-11 21:02:53 -0500

    wtf, seriously

---
+++ heart/ReadMe.md
@@ -0,0 +1,3 @@
+Long story short. This is what can happen when you run a PCA when practically anything else would have been better.  You can't spell heart without a rat.
+
+![]('heart/heart_rat.svg')
---
+++ heart/heart-shaped-uniformity.R
@@ -0,0 +1,30 @@
+library(ggplot2); library(lazerhawk); library(cowplot)
+# run commented part for the heart
+# set.seed(8675309)
+# N = 10000
+# 
+# A = matrix(c(2,3,2,1), 2)*.3
+# Suni = matrix(runif(2*N, min = -1), ncol = 2)
+# Xuni = Suni %*% A
+# 
+# pc_res = psych::principal(Xuni, 2)$scores
+# 
+# qplot(pc_res[,1], pc_res[,2], color=I('#FF000005'), size=I(10)) +
+#   lims(x=c(-3,3), y=c(-3,3)) +
+#   labs(x='', y='') +
+#   scale_x_reverse() +
+#   coord_polar(theta = 'x', start=pi, direction = 1) +
+#   theme_void()
+# 
+# ggsave('heart/heart.png')
+
+img1 <- png::readPNG('heart/heart.png')
+img2 <- png::readPNG('heart/brat.png')
+
+qplot() +
+  lims(x=c(-3,3), y=c(-3,3)) +
+  draw_image(img1, x=-1, y=-1, scale=10) +
+  draw_image(img2, x=-1, y=-1.65) +
+  theme_void()
+
+ggsave('heart/heart_rat.svg')
